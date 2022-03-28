df178a96f5233efce5634819a1bc814ff4d49ea2
radenadhyaksa <raksa69@gmail.com>
2022-03-28 14:05:19 +0700

    update widget_test

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(FirstScreen());
+  runApp(MyApp());
 }
 
-class FirstScreen extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Navigation & Routing'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              child: Text('Go to Second Screen'),
-              onPressed: () {},
-            ),
-            ElevatedButton(
-              child: Text('Navigation with Data'),
-              onPressed: () {},
-            ),
-            ElevatedButton(
-              child: Text('Return Data from Another Screen'),
-              onPressed: () {},
-            ),
-            ElevatedButton(
-              child: Text('Replace Screen'),
-              onPressed: () {},
-            ),
-          ],
-        ),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => FirstScreen(),
+        '/secondScreen': (context) => SecondScreen(),
+        '/secondScreenWithData': (context) =>
+            SecondScreenWithData(ModalRoute.of(context)?.settings.arguments as String),
+        '/returnDataScreen': (context) => ReturnDataScreen(),
+        '/replacementScreen': (context) => ReplacementScreen(),
+        '/anotherScreen': (context) => AnotherScreen(),
+      },
     );
   }
 }
- 
-class SecondScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
- 
-class SecondScreenWithData extends StatelessWidget {
-  final String data;
- 
-  const SecondScreenWithData(this.data);
- 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
- 
-class ReturnDataScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
- 
-class ReplacementScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
- 
-class AnotherScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
