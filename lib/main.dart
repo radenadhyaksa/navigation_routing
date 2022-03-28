2aed9618a9bcb960eaa9e663282f550a0acd797b
radenadhyaksa <raksa69@gmail.com>
2022-03-28 14:00:11 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,75 @@ void main() {
   runApp(const MyApp());
 }
 
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigation & Routing'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text('Go to Second Screen'),
+              onPressed: () {},
+            ),
+            ElevatedButton(
+              child: Text('Navigation with Data'),
+              onPressed: () {},
+            ),
+            ElevatedButton(
+              child: Text('Return Data from Another Screen'),
+              onPressed: () {},
+            ),
+            ElevatedButton(
+              child: Text('Replace Screen'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+ 
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+ 
+class SecondScreenWithData extends StatelessWidget {
+  final String data;
+ 
+  const SecondScreenWithData(this.data);
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+ 
+class ReturnDataScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+ 
+class ReplacementScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+ 
+class AnotherScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
