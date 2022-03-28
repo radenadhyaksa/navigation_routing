7fc388bd18695f1cc2bcd8e2e02c841c479ead46
radenadhyaksa <raksa69@gmail.com>
2022-03-28 14:03:53 +0700

    add file first_screen.dart

---
+++ lib/first_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigation & Routing'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text('Go to Second Screen'),
+              onPressed: () {},
+            ),
+            ElevatedButton(
+              child: Text('Navigation with Data'),
+              onPressed: () {},
+            ),
+            ElevatedButton(
+              child: Text('Return Data from Another Screen'),
+              onPressed: () {},
+            ),
+            ElevatedButton(
+              child: Text('Replace Screen'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class SecondScreenWithData extends StatelessWidget {
+  final String data;
+
+  const SecondScreenWithData(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class ReturnDataScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class ReplacementScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class AnotherScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
