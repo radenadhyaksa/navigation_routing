cefdc9ee2a27a555e54ae77b6704818f2b46694d
radenadhyaksa <raksa69@gmail.com>
2022-03-28 14:59:31 +0700

    update firstscreen - build Textfield dan Button

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -80,10 +80,43 @@ class SecondScreenWithData extends StatelessWidget {
   }
 }
 
-class ReturnDataScreen extends StatelessWidget {
+//class Return data screen
+class ReturnDataScreen extends StatefulWidget {
+  @override
+  _ReturnDataScreenState createState() => _ReturnDataScreenState();
+}
+
+class _ReturnDataScreenState extends State<ReturnDataScreen> {
+  final _textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                controller: _textController,
+                decoration: InputDecoration(labelText: 'Enter your name'),
+              ),
+            ),
+            ElevatedButton(
+              child: Text('Send'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
   }
 }
 
