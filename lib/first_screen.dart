f305da54d0a746550f4996db92af7c0a1816b415
radenadhyaksa <raksa69@gmail.com>
2022-03-28 14:40:29 +0700

    delete secondScreen.dart

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -39,7 +39,16 @@ class FirstScreen extends StatelessWidget {
 class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Back'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
   }
 }
 
